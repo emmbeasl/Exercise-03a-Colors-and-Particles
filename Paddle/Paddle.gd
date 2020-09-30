86d8837ddf5ebbf82576beb494cb12c20e891770
emmbeasl <70243384+emmbeasl@users.noreply.github.com>
2020-09-29 22:08:14 -0400

    Completes Game

--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -1,3 +1,4 @@
+
 extends KinematicBody2D
 
 onready var paddle_size = $Color.rect_size
@@ -11,15 +12,15 @@ func _ready():
 
 func update_color():
 	if HUD.color_paddle:
-		pass
+		$Color.color = Color8(173,181,189)
 	else:
-		pass
+		$Color.color = Color(1,1,1,1)
 
 func emit_particle(pos):
 	if HUD.particle_paddle:
-		pass
-	else:
-		pass
+		$Particles2D.global_position = pos
+		$Particles2D.emitting = true
+
 
 func _on_HUD_changed():
 	update_color()
